24a6bed99502afd829722bb91eab57f0899acf25
Pischulina Olga <pischulinaolvl@yandex.ru>
2025-12-17 01:43:57 +0400

    Техническое задание проекта №7. Удалила пробел

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -17,7 +17,7 @@ public class WordleDictionaryLoader {
     public static WordleDictionary getWordleDictionary(String fileName) {
 
         List<String> words = new ArrayList<>();
-        try ( BufferedReader br = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8))) {
             while (br.ready()) {
                 String line = br.readLine();
                 if (line.length() == 5) {

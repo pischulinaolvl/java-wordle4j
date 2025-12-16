fe9a5f9940f0f789670374ba3cc65418cc92d8a6
Pischulina Olga <pischulinaolvl@yandex.ru>
2025-12-17 01:48:46 +0400

    Техническое задание проекта №7. Код-ревью

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -19,8 +18,9 @@ import java.util.Scanner;
 public class Wordle {
 
     public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        try (PrintWriter fileLog = new PrintWriter (new FileWriter("C:\\Users\\pischulinaov\\IdeaProjects\\java-wordle4j\\_Logs\\log.txt", StandardCharsets.UTF_8))){
+        try (PrintWriter fileLog = new PrintWriter (new FileWriter("C:\\Users\\pischulinaov\\IdeaProjects\\java-wordle4j\\_Logs\\log.txt", StandardCharsets.UTF_8))) {
 
             WordleDictionary wordleDictionary = WordleDictionaryLoader.getWordleDictionary("C:\\Users\\pischulinaov\\IdeaProjects\\java-wordle4j\\words_ru.txt");
             fileLog.println("Получен справочник слов для игры");
@@ -31,34 +31,33 @@ public class Wordle {
             String resultCheckWord = "";
 
             fileLog.println("Запущен цикл по обработке попыток игрока");
-            while (wordleGame.checkSteps()){
-                System.out.println("Попытка " + (wordleGame.getSteps()+1) + ". Введите слово");
+            while (wordleGame.checkSteps()) {
+                System.out.println("Попытка " + (wordleGame.getSteps() + 1) + ". Введите слово");
                 String newWord = scanner.nextLine();
                 try {
-                    if (newWord.isEmpty()){
+                    if (newWord.isEmpty()) {
                         newWord = wordleGame.giveHint(fileLog);
-                        System.out.println("Подсказка: "+newWord);
+                        System.out.println("Подсказка: " + newWord);
                     }
                     resultCheckWord = wordleGame.tryWord(newWord, fileLog);
                     System.out.println("Результат проверки слова: " + resultCheckWord);
-                    if (resultCheckWord.equals("+++++")){
+                    if (resultCheckWord.equals("+++++")) {
                         break;
                     }
                     System.out.println();
-                }
-                catch (WordleGameException e) {
+                } catch (WordleGameException e) {
                     System.out.println(e.getMessage());
                 }
             }
 
-            if (resultCheckWord.equals("+++++")){
+            if (resultCheckWord.equals("+++++")) {
                 System.out.println("Поздравляем с победой! Вы отгадали слово.");
             } else {
-                System.out.println("Игра завершена. Было загадано слово "+wordleGame.getAnswer() + ".");
+                System.out.println("Игра завершена. Было загадано слово " + wordleGame.getAnswer() + ".");
             }
         }
         catch (IOException e) {
-            System.out.println( "Произошла ошибка во время записи файла.");
+            System.out.println("Произошла ошибка во время записи файла.");
         }
     }
 }
